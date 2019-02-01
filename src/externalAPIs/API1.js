abece0cfde04a67cf2a023ce43f4c03d21c80e55
Abhinav Kumar <kumarabhinav75@gmail.com>
2019-02-01 09:25:28 +0530

    Can get data from external API1

--- src/externalAPIs/API1.js
+++ src/externalAPIs/API1.js
@@ -4,7 +4,7 @@ const API1 = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBook
 
 let dataReceived;
 
-const getData = () =>{
+const getData = () => {
   request(API1, { json: true }, (err, res, body) => {
     if (err) {
       return console.log(err);
