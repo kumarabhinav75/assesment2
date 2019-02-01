d829da11c79b551fc50b3952b0be45aa5dc29ed8
Abhinav Kumar <kumarabhinav75@gmail.com>
2019-02-01 09:24:34 +0530

    Initial Commit

---
+++ src/externalAPIs/API1.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+const API1 = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks';
+
+let dataReceived;
+
+const getData = () =>{
+  request(API1, { json: true }, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    dataReceived = body;
+    console.log(dataReceived);
+  });
+};
+getData();
